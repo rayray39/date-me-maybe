f16df81ad717568e2755ac4ea4466b51e845de13
rayray39 <raynerlimfy@gmail.com>
2025-04-13 22:33:34 +0800

    MainPage.tsx: Update styling of main page components

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,3 +1,6 @@
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 
 function MainPage() {
 
@@ -5,13 +8,28 @@ function MainPage() {
         console.log('starting test...');
     }
 
-    return <>
-        <h1>Date Rayner, maybe</h1>
+    return <Stack sx={{
+            padding:'20px'
+        }}>
+            <Box>
+                <h1>🤭</h1>
+                <h1>Date Rayner, maybe</h1>
+                <h2 style={{fontWeight:'normal'}}>find out if you're compatible with me or if you just wanna know more about me!</h2>
+            </Box>
 
-        <div>find out if you're compatible with me or if you just wanna know more about me!</div>
-
-        <button onClick={handleStart}>start</button>
-    </>
+            <Box sx={{
+                position:'absolute',
+                bottom:90,
+                left:0,
+                right:0,
+                px:10
+            }}>
+                <Button sx={{
+                    height:"50px",
+                    fontSize:'18px'
+                }} variant="contained" disableElevation fullWidth onClick={handleStart}>{'start'}</Button>
+            </Box>
+        </Stack>
 }
 
 export default MainPage
