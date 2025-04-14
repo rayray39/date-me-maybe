4cb428147dd25d3a8dc4a74ae0c241af2fd1d6f0
rayray39 <raynerlimfy@gmail.com>
2025-04-14 10:18:24 +0800

    App.tsx: Add react router dom to navigate to diff pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import MainPage from "./MainPage"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RsIntention from "./RsIntention";
+
 
 function App() {
-    return <>
-        <MainPage />
-    </>
+    return (
+        <Router>
+          <Routes>
+            <Route path="/" element={<MainPage />} />
+            <Route path="/rs-intentions" element={<RsIntention />} />
+          </Routes>
+        </Router>
+      );
 }
 
 export default App
