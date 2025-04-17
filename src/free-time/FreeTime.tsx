e7e11c5fedac586ba950a58e95903bf0ca8d3da1
rayray39 <raynerlimfy@gmail.com>
2025-04-17 17:35:42 +0800

    FreeTime.tsx: Add textfield for user response

---
+++ src/free-time/FreeTime.tsx
@@ -0,0 +1,64 @@
+import Alert from "@mui/material/Alert"
+import Box from "@mui/material/Box"
+import Snackbar from "@mui/material/Snackbar"
+import Stack from "@mui/material/Stack"
+import { useState } from "react";
+import CustomButton from "../CustomButton";
+import TextField from "@mui/material/TextField";
+
+function FreeTime() {
+    const [response, setResponse] = useState<string>('');
+    const [openAlert, setOpenAlert] = useState<boolean>(false);
+
+    const handleShowAlert = () => {
+        // opens the snackbar with alert
+        setOpenAlert(true);
+    };
+
+    const handleClose = () => {
+        // closes the snackbar with alert
+        setOpenAlert(false);
+    };
+
+    const nextHandler = () => {
+        if (response === '') {
+            // if nothing is typed into the text field
+            handleShowAlert();
+            console.log('response is empty!');
+            return;
+        }
+        console.log(`${response}`);
+        console.log('completed Free time... proceeding on');
+    }
+
+    return <Stack sx={{
+        px:'20px'
+    }}>
+        <Box>
+            <h1>🏂</h1>
+            <h1>What do you like to do in your free time?</h1>
+        </Box>
+
+        <TextField value={response} onChange={(event) => setResponse(event.target.value)}
+            sx={{
+                marginTop:'20px',
+                mx: { xs: 2, sm: 10, md: 40 }
+            }}
+            id="outlined-basic" label="Your response" variant="outlined" multiline rows={4}/>
+
+        <Snackbar
+            open={openAlert}
+            autoHideDuration={3000}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        >
+            <Alert onClose={handleClose} severity="error" variant="filled">
+                Fill in something before proceeding!
+            </Alert>
+        </Snackbar>
+
+        <CustomButton content="next" onClickHandler={nextHandler} />
+    </Stack>
+}
+
+export default FreeTime
