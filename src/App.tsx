ed580e4112fdfd14b164408c47cce33764adb278
rayray39 <raynerlimfy@gmail.com>
2025-04-18 15:48:00 +0800

    App.tsx: Add comments and new routes to bad day pages

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { useState } from "react";
 import MyFreeTime from "./free-time/MyFreeTime";
 import MvSong from "./mv-song/MvSong";
 import MyMvSong from "./mv-song/MyMvSong";
+import BadDay from "./bad-day/BadDay";
+import MyBadDay from "./bad-day/MyBadDay";
 
 
 function App() {
@@ -28,14 +30,22 @@ function App() {
         <Router>
           <Routes>
             <Route path="/" element={<MainPage />} />
+
+            {/* rs intentions */}
             <Route path="/rs-intentions" element={<RsIntention onAnswer={(answer:string) => handleAnswer('rs-intention', answer)} />} />
             <Route path='/rs-intentions/:type' element={<RsIntentionsResult />} />
 
+            {/* what do you do in your free time */}
             <Route path="/free-time" element={<FreeTime onAnswer={(answer:string) => handleAnswer('free-time', answer)} />} />
             <Route path="/my-free-time" element={<MyFreeTime />} />
 
+            {/* song that makes you feel like in a MV */}
             <Route path="/mv-song" element={<MvSong onAnswer={(answer:string) => handleAnswer('mv-song', answer)} />} />
             <Route path="/my-mv-song" element={<MyMvSong />} />
+
+            {/* what do you want from partner after a bad day */}
+            <Route path="/bad-day" element={<BadDay onAnswer={(answer:string) => handleAnswer('bad-day', answer)} />} />
+            <Route path="/my-bad-day" element={<MyBadDay />} />
           </Routes>
 
             {/* <button onClick={showResponses}>click me</button> */}
