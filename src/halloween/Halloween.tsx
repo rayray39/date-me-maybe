47a39403713d5ce756aaa1f2ab3f02613a1f8e26
rayray39 <raynerlimfy@gmail.com>
2025-04-19 23:32:04 +0800

    Halloween.tsx: Add halloween costume question page

---
+++ src/halloween/Halloween.tsx
@@ -0,0 +1,69 @@
+import Alert from "@mui/material/Alert"
+import Box from "@mui/material/Box"
+import Snackbar from "@mui/material/Snackbar"
+import Stack from "@mui/material/Stack"
+import CustomButton from "../CustomButton"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { TextField } from "@mui/material"
+
+function Halloween({ onAnswer }:{ onAnswer:(answer:string) => void }) {
+    const navigate = useNavigate();
+
+    const [response, setResponse] = useState<string>('');
+
+    const [openAlert, setOpenAlert] = useState<boolean>(false);
+
+    const handleClose = () => {
+        // closes the snackbar and alert
+        setOpenAlert(false);
+    }
+
+    const handleShowAlert = () => {
+        // opens the snackbar with alert
+        setOpenAlert(true);
+    };
+
+    const nextHandler = () => {
+        if (response === '') {
+            // if text field is empty
+            handleShowAlert();
+            return;
+        }
+        console.log(response);
+        console.log('completed Halloween... proceeding on');
+        onAnswer(response);
+        navigate(`/my-halloween`);
+    }
+
+    return <Stack sx={{
+        px:'20px'
+    }}>
+        <Box>
+            <h1>🧜🧟</h1>
+            <h1>What would be our couple halloween costume/fit idea?</h1>
+        </Box>
+
+        <TextField value={response} onChange={(event) => setResponse(event.target.value)}
+                sx={{
+                    marginTop:'20px',
+                    mx: { xs: 2, sm: 10, md: 40 }
+                }}
+                id="halloween-textfield" label="Your response" variant="outlined" multiline rows={4}/>
+
+        <Snackbar
+            open={openAlert}
+            autoHideDuration={3000}
+            onClose={handleClose}
+            anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        >
+            <Alert onClose={handleClose} severity="error" variant="filled">
+                Select an option before proceeding!
+            </Alert>
+        </Snackbar>
+
+        <CustomButton content="next" onClickHandler={nextHandler} />
+    </Stack>
+}
+
+export default Halloween
---
+++ src/halloween/MyHalloween.tsx
@@ -0,0 +1,44 @@
+import Stack from "@mui/material/Stack"
+import CustomButton from "../CustomButton";
+import { useNavigate } from "react-router-dom";
+import Box from "@mui/material/Box";
+
+function MyShowLove() {
+    const navigate = useNavigate();
+
+    const handleContinue = () => {
+        console.log("continuing...");
+        navigate('/halloween');
+    }
+
+    return <Stack sx={{
+        px:'20px'
+    }}>
+        <h1>My halloween costume/couple fit idea</h1>
+
+        <h3 style={{fontWeight:'normal'}}>
+            I will dress up as Mo Salah🧔🏽 and you can dress up as Erling Haaland👱🏻‍♀️⚽️.<br/>
+            I will dress as Lewis Hamilton👦🏾 and you can be Max Verstappen🧑🏻🏎.
+        </h3>
+
+        <Box sx={{
+            width: {
+                xs: '80%',    
+                sm: '60%',    
+                md: '50%',    
+                lg: '30%',    
+            },
+            mx: 'auto',
+        }}>
+            <img style={{
+                display:'block',
+                width:'100%',
+                height:'auto',
+            }} src="/dancing.jpg" alt="virtual-hugs" />
+        </Box>
+
+        <CustomButton content="continue" onClickHandler={handleContinue} />
+    </Stack>
+}
+
+export default MyShowLove
