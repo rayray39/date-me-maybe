2561b9787543978e7b7feda510ab39bb215a8b4e
rayray39 <raynerlimfy@gmail.com>
2025-04-21 19:55:49 +0800

    MyRsAdvice.tsx: Update content

--- src/rs-advice/MyRsAdvice.tsx
+++ src/rs-advice/MyRsAdvice.tsx
@@ -13,10 +13,13 @@ function MyRsAdvice() {
     return <Stack sx={{
         px:'20px'
     }}>
-        <h1>My RS advice to ah boy Rayner</h1>
+        <h1>Advice to myself🦍🦦</h1>
 
         <h3 style={{fontWeight:'normal'}}>
-            
+            "Just because all your friends are in rs, doesn't mean you have to rush it. Be kind to others but most
+            importantly, be kind to yourself. You are so much more than the girls who ghosted you and rejected you. 
+            Take it easy on yourself, don't beat yourself up if things don't end up the way you want them to be. Don't 
+            do things that go against your values, be patient and know that the right one will show up one day!"
         </h3>
 
         <CustomButton content="continue" onClickHandler={handleContinue} />
